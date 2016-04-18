1c685f8e9eeee2730ce5643353d6138cc698fdfe
Solid Rhino <SolidRhino@users.noreply.github.com>
2016-04-18 17:41:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ git pull > /dev/null 2>&1
 sed -i -e "s#local_path.*#local_path      = /downloads#g" /app/config.ini
 #sed -i -e "s#email .*#email           ${EMAIL}#g" /app/config.ini
 #sed -i -e "s#password.*#password        ${PASSWORD}#g" /app/config.ini
-sed -i 's/^email*=/email = '"$EMAIL"'/g' /app/config.ini
-sed -i 's/^password*=/password = '"$PASSWORD"'/g' /app/config.ini
+sed -i -e 's/^email*= email/email = '"$EMAIL"'/g' /app/config.ini
+sed -i -e 's/^password*= password/password = '"$PASSWORD"'/g' /app/config.ini
 cat /app/config.ini
 #exec php /app/start.php
