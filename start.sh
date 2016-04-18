394af8e5e10c589101573d2cd0a92a6fa1cc9e23
Solid Rhino <SolidRhino@users.noreply.github.com>
2016-04-18 16:24:41 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,11 +6,10 @@ apk -q upgrade
 composer -q self-update
 composer -q install
 git pull > /dev/null 2>&1
-sed -i 's/^local_path*=/local_path = ${EMAIL}/g'
 sed -i -e "s#local_path.*#local_path      = /downloads#g" /app/config.ini
 #sed -i -e "s#email .*#email           ${EMAIL}#g" /app/config.ini
 #sed -i -e "s#password.*#password        ${PASSWORD}#g" /app/config.ini
-sed -i 's/^email*=/email = ${EMAIL}/g' /app/config.ini
-sed -i 's/^password*=/password = ${PASSWORD}/g' /app/config.ini
+sed -i 's/^email*=/email = "${EMAIL}"/g' /app/config.ini
+sed -i 's/^password*=/password = "${PASSWORD}"/g' /app/config.ini
 cat /app/config.ini
 #exec php /app/start.php
