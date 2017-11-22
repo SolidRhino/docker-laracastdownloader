4c61ad5f9501052d8fa4e882e25881b32e077957
Solid Rhino <SolidRhino@users.noreply.github.com>
2017-11-22 01:47:53 +0100

    Update start.sh
    Update start script

--- start.sh
+++ start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "Updating..."
 cd /app
-apk -q update
-apk -q upgrade
-composer -q self-update
-composer -q install
+apt-get update && apt-get upgrade
+php composer.phar -q self-update
+php composer.phar -q install
 git pull > /dev/null 2>&1
-sed -i -e "s#local_path.*#local_path      = /downloads#g" /app/config.ini
+sed -i -e "s#local_path.*#local_path      = /downloads#g" /app/.env
 #sed -i -e "s#email .*#email           ${EMAIL}#g" /app/config.ini
 #sed -i -e "s#password.*#password        ${PASSWORD}#g" /app/config.ini
-sed -i -e 's/^email*= email/email = '"$EMAIL"'/g' /app/config.ini
-sed -i -e 's/^password*= password/password = '"$PASSWORD"'/g' /app/config.ini
+sed -i -e 's/^email*= email/email = '"$EMAIL"'/g' /app/.env
+sed -i -e 's/^password*= password/password = '"$PASSWORD"'/g' /app/.env
 cat /app/config.ini
 #exec php /app/start.php
