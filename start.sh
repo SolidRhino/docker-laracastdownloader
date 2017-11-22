03acc079850d4c950ee4e839e6735302d2343515
Solid Rhino <SolidRhino@users.noreply.github.com>
2017-11-22 23:49:52 +0100

    Update start.sh
    Don't update system and fix sed file location

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Updating..."
 cd /app
-apt-get update && apt-get -y upgrade
+#apt-get update && apt-get -y upgrade
 php composer.phar -q self-update
 php composer.phar -q install
 git pull > /dev/null 2>&1
-sed -i -e "s#local_path.*#local_path      = /downloads#g" /app/.env
+sed -i -e "s#local_path.*#local_path      = /downloads#g" /.env
 #sed -i -e "s#email .*#email           ${EMAIL}#g" /app/config.ini
 #sed -i -e "s#password.*#password        ${PASSWORD}#g" /app/config.ini
-sed -i -e 's/^email*= email/email = '"$EMAIL"'/g' /app/.env
-sed -i -e 's/^password*= password/password = '"$PASSWORD"'/g' /app/.env
+sed -i -e 's/^email*= email/email = '"$EMAIL"'/g' /.env
+sed -i -e 's/^password*= password/password = '"$PASSWORD"'/g' /.env
 cat /app/.env
 #exec php /app/start.php
