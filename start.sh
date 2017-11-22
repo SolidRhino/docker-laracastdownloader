6d1a8110c3c3c5701562ff95c6f1e10ef59eb113
Solid Rhino <SolidRhino@users.noreply.github.com>
2017-11-22 02:45:32 +0100

    Update start.sh
    Fix cat and self upgrade

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Updating..."
 cd /app
-apt-get update && apt-get upgrade
+apt-get update && apt-get -y upgrade
 php composer.phar -q self-update
 php composer.phar -q install
 git pull > /dev/null 2>&1
@@ -10,5 +10,5 @@ sed -i -e "s#local_path.*#local_path      = /downloads#g" /app/.env
 #sed -i -e "s#password.*#password        ${PASSWORD}#g" /app/config.ini
 sed -i -e 's/^email*= email/email = '"$EMAIL"'/g' /app/.env
 sed -i -e 's/^password*= password/password = '"$PASSWORD"'/g' /app/.env
-cat /app/config.ini
+cat /app/.env
 #exec php /app/start.php
